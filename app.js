f398ea8f29db6272cefbe2fdd375cda54e29a4f9
Tanakorn Muwinitkul <tanakornfmu@gmail.com>
2022-08-25 11:45:25 +0700

    add cors config

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("./config");
 const app = require('./api/index.js')
+const cors = require("cors");
 
 // middleware
+app.use(cors());
 
 const boot = async () => {
     await mongoose
